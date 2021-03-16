fe11fae60352b0cbdff0a688353c13317e180d94
EdmundsGrimbergs <edmunds.grimbergs@gmail.com>
2021-03-16 14:29:32 +0200

    12comit

---
+++ app.js
@@ -0,0 +1,32 @@
+const list = document.querySelector(e){
+
+//delete books
+list.addEventLister('click',function(e){
+    if(e.target.className == 'dlete'){
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
+});
+
+// add books
+const addForm = document.forms['add-book'];
+addForm.addEventLister('subit',function(e){
+    e.preventDefult();
+    const value =addForm.querySelector('input[type="text"]').value
+
+    //crate elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBth = document.createElement('span');
+
+    // add contenet
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    //append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
+});
+
